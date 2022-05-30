af7eec4303661c16d4bf539b4cad25c790eacf30
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2022-05-30 23:15:04 +0530

    To find Number is Prime or Not

---
+++ NumberIsPrime.java
@@ -0,0 +1,25 @@
+package PepCoding;
+
+import java.util.Scanner;
+
+public class NumberIsPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t= sc.nextInt();
+        for(int i =0;i<t;i++)
+        {
+            int n = sc.nextInt();
+            int count=0;
+            for(int div=2;div*div<=n;div++) {
+                if (n % div == 0) {
+                    count++;
+                }
+            }
+                if (count == 0) {
+                    System.out.println("Prime");
+                } else {
+                    System.out.println("Not Prime");
+                }
+        }
+    }
+}
